68ef14629746a949cb03a09c1567eae899c5ec97
Bekadil <bekadilseksenbaev03@gmail.com>
2023-07-18 03:28:03 +0600

    add some functionals

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,48 @@
 <script>
 export default {
-
+    data: () => ({
+        Title:'Погодное приложение',
+        city:'',
+        error:''
+    }),
+    methods:{
+        getWeather(){
+            if(this.city.trim().length<2){
+               this.error = 'Нужно название более одного символа'
+                return false
+            }
+        }
+    },
+    computed:{
+        cityName(){
+            return '"'+this.city+'"'
+        }
+    }
 }
 </script>
 
 <template>
-  <div class="wrapper">
-      <h1>Погодное приложенме</h1>
-      <p>Узнать погоду в вашем городе</p>
-      <input type="text" name="" id="" placeholder="Введите название города">
-      <button>Получить погоду</button>
-  </div>
+    <div class="wrapper">
+        <h1>{{Title}}</h1>
+        <p>Узнать погоду в {{city==""?"вашем городе":cityName}}</p>
+        <input
+            v-model="city"
+            @keydown.enter="getWeather()"
+            type="text" name="" id="" placeholder="Введите название города">
+        <button
+            v-show="city!==''"
+            @click="getWeather()"
+        >Получить погоду</button>
+        <p class="error">{{error}}</p>
+    </div>
 </template>
 
 <style scoped>
-.wrapper{
+.error{
+    color: #d03939;
+    font-size: 14px;
+}
+.wrapper {
     width: 900px;
     height: 500px;
     border-radius: 50px;
@@ -23,13 +51,16 @@ export default {
     color: #fff;
     text-align: center;
 }
-.wrapper h1{
+
+.wrapper h1 {
     margin-top: 50px;
 }
-.wrapper p{
+
+.wrapper p {
     margin-top: 20px;
 }
-.wrapper input{
+
+.wrapper input {
     margin-top: 30px;
     background: transparent;
     border: 0;
@@ -39,21 +70,24 @@ export default {
     padding: 5px 8px;
     outline: none;
 }
-.wrapper input:focus{
+
+.wrapper input:focus {
     border-bottom-color: #60296c;
 }
-.wrapper button{
+
+.wrapper button {
     background: #da57ff;
-    color:#fff;
+    color: #fff;
     font-weight: 600;
     border-radius: 10px;
     border: 1px solid #da57ff;
     padding: 10px 15px;
     margin-left: 20px;
     cursor: pointer;
-    transition:transform 500ms ease;
+    transition: transform 500ms ease;
 }
-.wrapper button:hover{
+
+.wrapper button:hover {
     transform: scale(1.1) translateY(-2px);
 }
 </style>
